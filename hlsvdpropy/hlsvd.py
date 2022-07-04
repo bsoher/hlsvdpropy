fdaf521d39a4ed4c4c016643f3cec0d2d37d5cf6
Brian J Soher <bsoher@briansoher.com>
2022-07-04 17:34:35 -0400

    Tested Will Clarke's pull request fix for np.mat() deprecation.  Also found another deprecation warning for np.complex, changed to np.complex128

--- hlsvdpropy/hlsvd.py
+++ hlsvdpropy/hlsvd.py
@@ -149,7 +149,7 @@ def hlsvdpro(data, nsv_sought, m=None, sparse=False):
 
     k = min(k,len(s))               # number of singular values found
         
-    uk = np.mat(u[:, :k])           # trucated U matrix of rank K
+    uk = u[:, :k]                   # trucated U matrix of rank K
     ub = uk[:-1]                    # Uk with bottom row removed
     ut = uk[1:]                     # Uk with top row removed
 
@@ -245,7 +245,7 @@ def create_hlsvd_fids(result, npts, dwell, sum_results=False, convert=True):
 
     freqs, damps, areas, phase = result[2:6]
 
-    fids = np.zeros((len(freqs), npts), dtype=np.complex)
+    fids = np.zeros((len(freqs), npts), dtype=np.complex128)
     t = np.arange(npts) * dwell
     k = 1j * 2 * np.pi
 
